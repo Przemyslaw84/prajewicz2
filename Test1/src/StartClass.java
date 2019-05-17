99035551465b81c6b2d7b65c7e8a248271eee4bb
Przemek <przemek.rajewicz@gmail.com>
2019-05-17 17:34:54 +0200

    trzecia zmiana

--- Test1/src/StartClass.java
+++ Test1/src/StartClass.java
@@ -3,7 +3,7 @@ public class StartClass {
 
 	public static void main(String[] args) {
 	System.out.println("Dzieñ dobry");
-	System.out.println("Dzo widzenia");
+	System.out.println("Dobranoc");
 	}
 
 }

7bd1a775c1c54c28df3eb7da3187266c393dee67
Przemek <przemek.rajewicz@gmail.com>
2019-05-17 17:44:32 +0200

    merg

--- Test1/src/StartClass.java
+++ Test1/src/StartClass.java
@@ -3,11 +3,9 @@ public class StartClass {
 
 	public static void main(String[] args) {
 	System.out.println("Dzieñ dobry");
-<<<<<<< HEAD
-	System.out.println("Dobranoc");
-=======
-	System.out.println("Dobry wieczór");
->>>>>>> 775234b7edb39de229a8fb52bf20911425bd3b5c
+ 
+	System.out.println("Dobranocka");
+ 
 	}
 
 }
